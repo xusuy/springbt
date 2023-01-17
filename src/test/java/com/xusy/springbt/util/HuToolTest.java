c555dd3a533814f1c9528003c22583905b68b747
xusuyou <1994xsy1994>
2023-01-17 10:07:43 +0800

    1

--- src/test/java/com/xusy/springbt/util/HuToolTest.java
+++ src/test/java/com/xusy/springbt/util/HuToolTest.java
@@ -3,6 +3,7 @@ package com.xusy.springbt.util;
 import cn.hutool.core.util.NumberUtil;
 import org.junit.Test;
 
+import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 /**
@@ -16,5 +17,19 @@ public class HuToolTest {
     public void calculate() {
         double eachIntegral = NumberUtil.div(50, 5, 1, RoundingMode.UP);// 相除：取一位小数 四舍五入
         System.out.println(eachIntegral);
+    }
+
+    // 数字相关
+    // double 去除末尾.0
+    private String convertDoubleToString(double num) {
+        BigDecimal bd = new BigDecimal(String.valueOf(num));
+        return bd.stripTrailingZeros().toPlainString();
+    }
+
+    @Test
+    public void removeEndZero() {
+        System.out.println(convertDoubleToString(0.0));
+        System.out.println(convertDoubleToString(1.0));
+        System.out.println(convertDoubleToString(1.10));
     }
 }
